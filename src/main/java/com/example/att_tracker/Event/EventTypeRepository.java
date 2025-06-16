2591f7bbf2c1006a3ba1bee22109366455d49b40
enochappiah <eappiah4@jhu.edu>
2025-06-15 20:02:07 -0400

    refactoring repo to only include frontend

--- src/main/java/com/example/att_tracker/Event/EventTypeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.att_tracker.Event;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventTypeRepository extends JpaRepository<EventType, Long> {
-}
